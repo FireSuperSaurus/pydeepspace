84c9c98c321768c3c6297c08114631e20388f026
Joel Bryla <joel@wiredlizard.com>
2019-01-21 15:34:02 +1100

    Implemented rev.sparkmax

--- components/climb.py
+++ components/climb.py
@@ -1,49 +1,76 @@
-import SparkMax
 from dataclasses import dataclass
+import rev
+
+
+class Lifts:
+    def __init__(self, front_motor: rev.CANSparkMax, back_motor: rev.CANSparkMax):
+        self.front, self.back = [
+            {
+                "motor": motor,
+                "encoder": motor.getEncoder(),
+                "pid_controller": motor.getPIDController(),
+            }
+            for motor in [front_motor, back_motor]
+        ]
+
+
+@dataclass
+class PID:
+    P: float
+    I: float
+    D: float
+    F: float
 
 
 class Climber:
-    @dataclass
-    class Lifts:
-        front: SparkMax
-        back: SparkMax
+    front_lift: rev.CANSparkMax
+    back_lift: rev.CANSparkMax
 
-    front_lift: SparkMax
-    back_lift: SparkMax
+    GROUND_OFFSET = 5  # motor rotations
 
-    ENCODER_TYPE = SparkMax.ENCODER_TYPE
-    COUNTS_PER_REV = int
-    METRES_PER_REV = 0.005
+    # TODO get values
+    # heights in metres
+    METRES_PER_REV = 0.002
 
-    UP_HEIGHT = int
-    DOWN_HEIGHT = int
+    UP_HEIGHT = 5
+    DOWN_HEIGHT = 5
 
     THRESHOLD = 0.01
 
     def __init__(self):
-        self.lifts = self.lifts(self.front_lift, self.back_lift)
+        # TODO tune values
+        self.up_PID = PID(0, 0, 0, 0)
+        self.down_PID = PID(0, 0, 0, 0)
 
-    def set_lift_height(self, lift, pos):
-        set_point = pos / self.METRES_PER_REV * self.COUNTS_PER_REV
-        lift.set(set_point)
+    def setup(self):
+        self.lifts = Lifts(self.front_lift, self.back_lift)
+
+    def on_disable(self):
+        self.front_lift.stopMotor()
+        self.back_lift.stopMotor()
+
+    def set_pid(self, lift, pid):
+        lift["pid_controller"].setP(pid.P)
+        lift["pid_controller"].setI(pid.I)
+        lift["pid_controller"].setD(pid.D)
+        lift["pid_controller"].setFF(pid.F)
+
+    def set_lift_height(self, lift, set_point_metres):
+        if set_point_metres - self.get_lift_height(lift) > 0:
+            self.set_pid(lift, self.up_PID)
+        else:
+            self.set_pid(lift, self.down_PID)
+
+        set_point = set_point_metres / self.METRES_PER_REV + self.GROUND_OFFSET
+        lift["pid_controller"].setReference(set_point, rev.ControlType.kPosition)
 
     def get_lift_height(self, lift):
-        pos = lift.get_pos()
-        return pos / self.COUNTS_PER_REV * self.METRES_PER_REV
+        pos = lift["encoder"].getPosition()
+        return (pos - self.GROUND_OFFSET) * self.METRES_PER_REV
 
-    def within_threshold(self, value, goal):
+    def get_lift_status(self, lift, goal):
+        value = self.get_lift_height(lift)
         return (goal + self.THRESHOLD) >= value >= (goal - self.THRESHOLD)
 
-    def both_legs_down(self):
-        self.set_lift_height(self.lifts.front, self.DOWN_HEIGHT)
-        self.set_lift_height(self.lifts.back, self.DOWN_HEIGHT)
-
-    def both_legs_down_status(self):
-        front = self.within_threshold(
-            self.get_lift_height(self.lifts.front), self.DOWN_HEIGHT
-        )
-        back = self.within_threshold(
-            self.get_lift_height(self.lifts.back), self.DOWN_HEIGHT
-        )
-
-        return front and back
+    def execute(self):
+        pass

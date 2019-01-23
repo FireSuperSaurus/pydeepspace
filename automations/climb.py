1e96d9ea04a8226d9c0433c79a6f91ff0aa58cb8
FireSuperSaurus <fiona.chau@thedropbears.org.au>
2019-01-23 20:42:16 +1100

    ._.

--- automations/climb.py
+++ automations/climb.py
@@ -9,7 +9,7 @@ class ClimbAutomation(StateMachine):
         self.engage()
 
 #driving off lvl 1
-    @timed_state(first=True, must_finish=True, duration=1.0, next_state="both_climbs_up")
+    @timed_state(first=True, must_finish=True, duration=1.0, next_state="both_climbs_down")
     def drive_off(self):
         self.both_motors.move()
 
@@ -17,37 +17,45 @@ class ClimbAutomation(StateMachine):
 
 #going up lvl 3
     @state(must_finish=True)
-    def both_climbs_up(self, initial_call):
+    def both_lifts_down(self, initial_call):
         if initial_call:
-            self.both_climbs.move(self.climb.MAX_REST_H)
-        if self.both_climbs.at_pos():
+            self.climber.set_lift_height(lift, set_point_metres)
+            self.get_lift_height(climber.lifts.front, climber.UP_HEIGHT)
+            self.get_lift_height(climber.lifts.back, climber.UP_HEIGHT)
+        if self.both_lifts.at_pos():
             self.next_state_now("drive_forward")
     
-    @timed_state(must_finish=True, duration=0.5, next_state="front_climb_down")
+    @state(must_finish=True)
     def drive_forward(self):
-        self.both_motors.move()
+        self.get_lift_status(climber.lifts.front)
+        self.climber.move_forward()
+        if self.is_touching_podium(climber.lifts.front):
+            self.next_state_now("front_lift_up")
 
     @state(must_finish=True)
-    def front_climb_down(self, initial_call):
+    def front_lift_up(self, initial_call):
         if initial_call:
-            self.both_motors.move()
-            self.front_climb.move(self.climb.MIN_REST_H)
-        if self.front_climb.at_pos():
+            self.climber.set_lift_height(lift, set_point_metres)
+            self.get_lift_height(climber.lifts.front, climber.DOWN_HEIGHT)
+        if self.front_lift.at_pos():
             self.next_state_now("going_forward")
 
-    @timed_state(must_finish=True, duration=0.5, next_state="back_climb_down")
+    @state(must_finish=True)
     def going_forward(self):
-        self.both_motors.move()
+        self.get_lift_status(climber.lifts.back)
+        self.climber.move_forward()
+        if self.is_touching_podium(climber.lifts.back):
+            self.next_state_now("back_lift_up")
 
     @state(must_finish=True)
-    def back_climb_down(self, initial_call):
+    def back_lift_up(self, initial_call):
         if initial_call:
-            self.both_motors.move()
-            self.back_climb.move(self.climb.MIN_REST_H)
-        if self.back_climb.at_pos():
+            self.climber.set_lift_height(lift, set_point_metres)
+            self.get_lift_height(climber.lifts.back, climber.DOWN_HEIGHT)
+        if self.back_lift.at_pos():
             self.next_state_now("finish_pos")
 
     @state(must_finish=True)
     def finish_pos(self):
-        self.both_motors.move(0.2)
+        self.climber.move_forward()
         self.done()

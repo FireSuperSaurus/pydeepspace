25849e164e16cb05bfea6e0d7530060a14ae60c8
Joel Bryla <joel@wiredlizard.com>
2019-01-25 13:48:20 +1100

    Reorganised climb component

--- components/climb.py
+++ components/climb.py
@@ -1,59 +1,59 @@
-from dataclasses import dataclass
 import rev
+import ctre
+import wpilib
+from utilities.pid import PID
 
 
-class Lifts:
-    def __init__(self, front_motor: rev.CANSparkMax, back_motor: rev.CANSparkMax):
-        self.front, self.back = [
-            {
-                "motor": motor,
-                "encoder": motor.getEncoder(),
-                "pid_controller": motor.getPIDController(),
-            }
-            for motor in [front_motor, back_motor]
-        ]
+class Lift:
+    lift_motor: rev.CANSparkMax
+    drive_motor: ctre.TalonSRX
 
+    left_limit_switch: wpilib.DigitalInput
+    right_limit_switch: wpilib.DigitalInput
 
-@dataclass
-class PID:
-    P: float
-    I: float
-    D: float
-    F: float
-
-
-class Climber:
-    front_lift: rev.CANSparkMax
-    back_lift: rev.CANSparkMax
-
+    # Lift Constants
     GROUND_OFFSET = 5  # motor rotations
 
     # TODO get values
     # heights in metres
-    METRES_PER_REV = 0.002
+    LIFT_METRES_PER_REV = 0.002
 
     UP_HEIGHT = 5
     DOWN_HEIGHT = 5
 
     THRESHOLD = 0.01
 
-    def __init__(self):
-        # TODO tune values
-        self.up_PID = PID(0, 0, 0, 0)
-        self.down_PID = PID(0, 0, 0, 0)
+    up_PID = PID(0, 0, 0, 0)
+    down_PID = PID(0, 0, 0, 0)
+
+    # Drive Constants
+    DRIVE_ENCODER_TYPE = ctre.FeedbackDevice.QuadEncoder
+
+    drive_PID = PID(0, 0, 0, 0)
 
     def setup(self):
-        self.lifts = Lifts(self.front_lift, self.back_lift)
+        self.lift_encoder = self.lift_motor.getEncoder()
+        self.lift_pid_controller = self.lift_motor.getPIDController()
+
+        self.limit_switches = [self.left_limit_switch, self.right_limit_switch]
+
+        self.drive_motor.configSelectedFeedbackSensor(
+            self.DRIVE_ENCODER_TYPE, 0, timeoutMs=10
+        )
+        self.drive_motor.config_kP(0, self.drive_PID.P, timeoutMs=10)
+        self.drive_motor.config_kI(0, self.drive_PID.I, timeoutMs=10)
+        self.drive_motor.config_kD(0, self.drive_PID.D, timeoutMs=10)
+        self.drive_motor.config_kF(0, self.drive_PID.F, timeoutMs=10)
 
     def on_disable(self):
-        self.front_lift.stopMotor()
-        self.back_lift.stopMotor()
+        self.stop_lift()
+        self.stop_wheels()
 
-    def set_pid(self, lift, pid):
-        lift["pid_controller"].setP(pid.P)
-        lift["pid_controller"].setI(pid.I)
-        lift["pid_controller"].setD(pid.D)
-        lift["pid_controller"].setFF(pid.F)
+    def set_pid(self, pid):
+        self.lift_pid_controller.setP(pid.P)
+        self.lift_pid_controller.setI(pid.I)
+        self.lift_pid_controller.setD(pid.D)
+        self.lift_pid_controller.setFF(pid.F)
 
     def set_lift_height(self, lift, set_point_metres):
         if set_point_metres - self.get_lift_height(lift) > 0:
@@ -61,16 +61,36 @@ class Climber:
         else:
             self.set_pid(lift, self.down_PID)
 
-        set_point = set_point_metres / self.METRES_PER_REV + self.GROUND_OFFSET
-        lift["pid_controller"].setReference(set_point, rev.ControlType.kPosition)
+        self.lift_set_point = (
+            set_point_metres / self.LIFT_METRES_PER_REV + self.GROUND_OFFSET
+        )
+        self.lift_motor.lift_pid_controller.setReference(
+            self.lift_set_point, rev.ControlType.kPosition
+        )
 
-    def get_lift_height(self, lift):
-        pos = lift["encoder"].getPosition()
-        return (pos - self.GROUND_OFFSET) * self.METRES_PER_REV
+    def get_lift_height(self):
+        pos = self.lift_encoder.getPosition()
+        return (pos - self.GROUND_OFFSET) * self.LIFT_METRES_PER_REV
 
-    def get_lift_status(self, lift, goal):
-        value = self.get_lift_height(lift)
-        return (goal + self.THRESHOLD) >= value >= (goal - self.THRESHOLD)
+    def stop_lift(self):
+        self.lift_set_point = None
+        self.lift_motor.lift_motor.stopMotor()
+
+    def get_lift_status(self):
+        lift_pos = self.get_lift_height()
+        return self.is_within_threshold(lift_pos, self.lift_set_point)
+
+    def is_within_threshold(self, pos, goal):
+        return (goal + self.THRESHOLD) >= pos >= (goal - self.THRESHOLD)
+
+    def move_wheels_forward(self):
+        self.drive_motor.set(ctre.ControlMode.PercentOutput, 0.5)
+
+    def stop_wheels(self):
+        self.drive_motor.set(ctre.ControlMode.PercentOutput, 0)
+
+    def is_touching_podium(self, lift):
+        return all([switch.get() for switch in self.limit_switches])
 
     def execute(self):
         pass
